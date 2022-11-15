f1b695aab0a167c5ae8cbcaa8c4b3cca9dd4d834
Namju Kim <cmsong111@naver.com>
2022-11-15 12:17:28 +0900

    use start.spring.io
    Gradle Projet
    java jdk17
    
    group -> com.galaxy.
    Artifact -> Restaurant-information-system
    
    add dependency
    + lombok
    + spring web
    + maria db
    + spring data jpa

---
+++ Restaurant-information-system/src/main/java/com/galaxy/Restaurantinformationsystem/RestaurantInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.Restaurantinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantInformationSystemApplication.class, args);
+	}
+
+}
---
+++ Restaurant-information-system/src/test/java/com/galaxy/Restaurantinformationsystem/RestaurantInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.Restaurantinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
